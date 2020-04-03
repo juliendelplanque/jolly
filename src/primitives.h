10f2d3cbb965cd5c058c9334ef60d6e91220f524
Julien Delplanque <julien.delplanque@live.be>
2020-04-03 20:09:47 +0200

    Reorganized headers, moved execute_primitive to vm.c.

--- src/primitives.h
+++ src/primitives.h
@@ -1,3 +1,7 @@
+#ifndef PRIMITIVES_H
+
+#define PRIMITIVES_H
+
 #include "memory.h"
 
 #define PRIMITIVE_ID_NOPE 0
@@ -5,3 +9,16 @@
 
 #define PRIMITIVE_OK_RESULT_CODE 0
 #define PRIMITIVE_FAILED_RESULT_CODE 1
+
+/**
+ * A primitive that always fails.
+ * Writes the PRIMITIVE_FAILED_RESULT_CODE at PRIMITIVE_RESULT_CODE_ADDRESS.
+ */
+void primitive_fail(unsigned char *memory);
+
+/**
+ * A primitive that does nothing.
+ */
+void primitive_nop(unsigned char *memory);
+
+#endif
--- src/vm.c
+++ src/vm.c
@@ -1,26 +1,34 @@
+#include "vm.h"
 #include "memory.h"
 #include "primitives.h"
 
 #include <stdlib.h>
 #include <stdio.h>
 
-#define INNER_LOOP_ITERATIONS 65536
-
-// Offsets to read an instruction.
-#define FROM_ADDRESS_HIGH_OFFSET 0
-#define FROM_ADDRESS_MIDDLE_OFFSET 1
-#define FROM_ADDRESS_LOW_OFFSET 2
-
-#define TO_ADDRESS_HIGH_OFFSET 3
-#define TO_ADDRESS_MIDDLE_OFFSET 4
-#define TO_ADDRESS_LOW_OFFSET 5
-
-#define JUMP_ADDRESS_HIGH_OFFSET 6
-#define JUMP_ADDRESS_MIDDLE_OFFSET 7
-#define JUMP_ADDRESS_LOW_OFFSET 8
-
 void print_pc_address(unsigned char *memory, unsigned char *pc){
     printf("pc = 0x%06X\n", ((unsigned)pc)-((unsigned)memory));
+}
+
+int execute_primitive(unsigned char *memory){
+    unsigned char primitive_id;
+    // Retrieve the id of the primitive to be executed.
+    primitive_id = memory[PRIMITIVE_CALL_ID_ADDRESS];
+    switch(primitive_id){
+        case(PRIMITIVE_ID_NOPE):
+            primitive_nop(memory);
+            break;
+        case(PRIMITIVE_ID_FAIL):
+            primitive_fail(memory);
+            break;
+        default: // In case no primitive is associated to an id, the call fails.
+            primitive_fail(memory);
+            break;
+    }
+    // Set the value of primitive to execute in next frame to PRIMITIVE_ID_NOPE
+    // like that, if the code sets no primitive to execute during next frame,
+    // nope primitive will be executed which is hand as it does nothing.
+    memory[PRIMITIVE_CALL_ID_ADDRESS] = PRIMITIVE_ID_NOPE;
+    return 0;
 }
 
 int inner_loop(unsigned char *memory, unsigned char *pc){
