9637097578648807534572679722ab86d7ac531b
Julien Delplanque <julien.delplanque@live.be>
2020-04-03 19:14:55 +0200

    Allow one to print PC, fixed macros usage and values.

--- src/vm.c
+++ src/vm.c
@@ -1,58 +1,77 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 #define WORD_SIZE 8
 #define DOUBLE_WORD_SIZE (WORD_SIZE * 2)
 #define INNER_LOOP_ITERATIONS 65536
 
-#define FROM_ADDRESS_LOW_OFFSET 0
+#define MAX_MEMORY_SIZE 0x1000008
+
+#define NEXT_FRAME_PC_HIGH_ADDRESS 0
+#define NEXT_FRAME_PC_MIDDLE_ADDRESS 1
+#define NEXT_FRAME_PC_LOW_ADDRESS 2
+
+#define FROM_ADDRESS_HIGH_OFFSET 0
 #define FROM_ADDRESS_MIDDLE_OFFSET 1
-#define FROM_ADDRESS_HIGH_OFFSET 2
+#define FROM_ADDRESS_LOW_OFFSET 2
 
-#define TO_ADDRESS_LOW_OFFSET 3
+#define TO_ADDRESS_HIGH_OFFSET 3
 #define TO_ADDRESS_MIDDLE_OFFSET 4
-#define TO_ADDRESS_HIGH_OFFSET 5
+#define TO_ADDRESS_LOW_OFFSET 5
 
-#define JUMP_ADDRESS_LOW_OFFSET 6
+#define JUMP_ADDRESS_HIGH_OFFSET 6
 #define JUMP_ADDRESS_MIDDLE_OFFSET 7
-#define JUMP_ADDRESS_HIGH_OFFSET 8
+#define JUMP_ADDRESS_LOW_OFFSET 8
 
+void print_pc_address(unsigned char *memory, unsigned char *pc){
+    printf("pc = 0x%06X\n", ((unsigned)pc)-((unsigned)memory));
+}
 
 int inner_loop(unsigned char *memory, unsigned char *pc){
     int i = INNER_LOOP_ITERATIONS;
     do {
         int from_address,to_address,jump_address;
         // Compute address to copy word from.
-        from_address = pc[FROM_ADDRESS_LOW_OFFSET] << DOUBLE_WORD_SIZE
+        from_address = pc[FROM_ADDRESS_HIGH_OFFSET] << DOUBLE_WORD_SIZE
             | pc[FROM_ADDRESS_MIDDLE_OFFSET] << WORD_SIZE
-            | pc[FROM_ADDRESS_HIGH_OFFSET];
+            | pc[FROM_ADDRESS_LOW_OFFSET];
         
         // Compute address to copy word to.
-        to_address = pc[TO_ADDRESS_LOW_OFFSET] << DOUBLE_WORD_SIZE 
+        to_address = pc[TO_ADDRESS_HIGH_OFFSET] << DOUBLE_WORD_SIZE 
             | pc[TO_ADDRESS_MIDDLE_OFFSET] << WORD_SIZE
-            | pc[TO_ADDRESS_HIGH_OFFSET];
+            | pc[TO_ADDRESS_LOW_OFFSET];
 
         // Copy word pointed by from_address to to_address.
         memory[to_address] = memory[from_address];
 
         // Compute next address for program counter: the jump address.
-        jump_address = pc[JUMP_ADDRESS_LOW_OFFSET] << DOUBLE_WORD_SIZE
+        jump_address = pc[JUMP_ADDRESS_HIGH_OFFSET] << DOUBLE_WORD_SIZE
             | pc[JUMP_ADDRESS_MIDDLE_OFFSET] << WORD_SIZE
-            | pc[JUMP_ADDRESS_HIGH_OFFSET];
+            | pc[JUMP_ADDRESS_LOW_OFFSET];
         
         // Update program counter according to jump_address (absolute jump).
         pc = memory + jump_address;
+        print_pc_address(memory, pc);
     } while (--i);
     return 0;
 }
 
 int execute_frame(unsigned char *memory){
     unsigned char *pc;
-    pc = memory + (memory[2]<<16 | memory[3]<<8 | memory[4]);
+    pc = memory + (memory[2] << DOUBLE_WORD_SIZE
+                    | memory[3] << WORD_SIZE
+                    | memory[4]);
     inner_loop(memory, pc);
     return 0;
 }
 
 int main (){   
-    unsigned char memory[0x1000008];
+    unsigned char *memory;
+    memory = (unsigned char *)malloc(MAX_MEMORY_SIZE);
+    if(memory == NULL){
+        fprintf(stderr, "Malloc failed.\n");
+    }
+    execute_frame(memory);
+    free(memory);
     return 0;
 }
