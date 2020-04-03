d0d4c5581e2f6663a4c5f99ea031addd37a4193d
Julien Delplanque <julien.delplanque@live.be>
2020-04-03 22:09:12 +0200

    Added macros and signatures of primitives to implement.

--- src/primitives.h
+++ src/primitives.h
@@ -10,6 +10,9 @@
 #define PRIMITIVE_OK_RESULT_CODE 0
 #define PRIMITIVE_FAILED_RESULT_CODE 1
 
+#define PRIMITIVE_NOT_READY 0
+#define PRIMITIVE_READY 1
+
 /**
  * A primitive that always fails.
  * Writes the PRIMITIVE_FAILED_RESULT_CODE at PRIMITIVE_RESULT_CODE_ADDRESS.
@@ -20,5 +23,9 @@ void primitive_fail(WORD *memory);
  * A primitive that does nothing.
  */
 void primitive_nop(WORD *memory);
+
+void primitive_put_char(WORD *memory);
+
+void primitive_save_snapshot(WORD *memory);
 
 #endif
