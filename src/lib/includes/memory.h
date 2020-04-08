687f17a124fb2885a41cbb82b5ecc32845cbed8d
Julien Delplanque <julien.delplanque@live.be>
2020-04-08 17:52:02 +0200

    Added doc.

--- src/lib/includes/memory.h
+++ src/lib/includes/memory.h
@@ -8,17 +8,56 @@
 
 #define MAX_MEMORY_SIZE 0x1000008
 
-// Memory layout for the VM.
-#define PC_HIGH_ADDRESS 0
-#define PC_MIDDLE_ADDRESS 1
-#define PC_LOW_ADDRESS 2
+/**
+ * Memory layout for the virtual machine.
+ */
 
-#define PRIMITIVE_IS_READY_ADDRESS 3
-#define PRIMITIVE_CALL_ID_ADDRESS 4
-#define PRIMITIVE_RESULT_CODE_ADDRESS 5
-#define PRIMITIVE_RESULT_POINTER_HIGH_ADDRESS 6
-#define PRIMITIVE_RESULT_POINTER_MIDDLE_ADDRESS 7
-#define PRIMITIVE_RESULT_POINTER_LOW_ADDRESS 8
+/**
+ * Address of the byte storing the most significant bits of the program counter.
+ */
+#define PC_HIGH_ADDRESS 0x000000
+
+/**
+ * Address of the byte storing the middle bits of the program counter.
+ */
+#define PC_MIDDLE_ADDRESS 0x000001
+
+/**
+ * Address of the byte storing the less significant bits of the program counter.
+ */
+#define PC_LOW_ADDRESS 0x000002
+
+/**
+ * Address of the byte storing a value that triggers primitive execution.
+ */
+#define PRIMITIVE_IS_READY_ADDRESS 0x000003
+
+/**
+ * Address of the byte storing a value that is the id of the primitive to
+ * execute.
+ */
+#define PRIMITIVE_CALL_ID_ADDRESS 0x000004
+
+/**
+ * Address of the byte storing a value that is the result code of the last
+ * primitive executed by the virtual machine.
+ */
+#define PRIMITIVE_RESULT_CODE_ADDRESS 0x000005
+
+/**
+ * Address of the byte storing the most significant bits of the result pointer.
+ */
+#define PRIMITIVE_RESULT_POINTER_HIGH_ADDRESS 0x000006
+
+/**
+ * Address of the byte storing the middle bits of the result pointer.
+ */
+#define PRIMITIVE_RESULT_POINTER_MIDDLE_ADDRESS 0x000007
+
+/**
+ * Address of the byte storing the less significant bits of the result pointer.
+ */
+#define PRIMITIVE_RESULT_POINTER_LOW_ADDRESS 0x000008
 
 #define NULL_MEMORY (WORD *) NULL
 
